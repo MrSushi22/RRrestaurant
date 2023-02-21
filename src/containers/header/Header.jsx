159c9383637d5830966e1d2ba2eca73db0c68153
andras nemeth <andras.nemeth02@gmail.com>
2023-02-21 14:48:30 +0100

    ai picture fix

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
+import ai from '../../assets/aihead.png';
 import './header.css';
 
 const Header = () => (
@@ -21,7 +21,7 @@ const Header = () => (
     </div>
 
     <div className="gpt3__header-image">
-      <img src={ai} />
+      <img src={aihead} />
     </div>
   </div>
 );
